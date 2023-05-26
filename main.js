2dc2d759f243071e2dee6f1075dd22518688ca84
June <sungj1207@gmail.com>
2023-05-27 01:25:15 +0900

    산술, 할당 연산자

--- main.js
+++ main.js
@@ -13,3 +13,12 @@ console.log(getType(123)) // [Number] [] 없애기 위해 slice
 console.log(getType(null)) // Null
 console.log(getType({})) // Object
 console.log(getType([])) // Array
+
+// 산술 연산자(arithmetic operator)
+console.log(7 % 5) // 나머지
+
+// 할당 연산자(assignment operator)
+let a = 2
+// a = a + 1
+a += 1 // * / 도 가능
+console.log(a)
