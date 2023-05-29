ef097ebb10d6ff2f3831a647f678adef86f083a4
June <sungj1207@gmail.com>
2023-05-29 19:02:06 +0900

    ES6 Classes

--- main.js
+++ main.js
@@ -325,3 +325,55 @@ console.log(neo.getFullName())
 //   }
 // }
 // timer.timeout()
+
+//-------------------------
+
+// 참고
+const kevin = {
+  name: 'Kevin',
+  normal: function () { // : function 생략해도 동작
+    console.log(this.name)
+  },
+  arrow: () => {
+    console.log(this.name)
+  }
+}
+
+kevin.normal() // Kevin
+kevin.arrow() // undifined
+
+// ES6 Classes
+
+// function User(first, last) {
+//   this.firstName = first
+//   this.lastName = last
+// }
+// User.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`
+// }
+// const june = new User('June', 'Lee')
+// const amy = new User('Amy', 'Clarke')
+// const neo = new User('Neo', 'Smith')
+
+// console.log(june)
+// console.log(amy.getFullName())
+// console.log(neo.getFullName())
+
+//-----------Class 사용하면 동일-------------
+
+// class User {
+//   constructor(first, last) {
+//     this.firstName = first
+//     this.lastName = last
+//   }
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`
+//   }
+// }
+// const june = new User('June', 'Lee')
+// const amy = new User('Amy', 'Clarke')
+// const neo = new User('Neo', 'Smith')
+
+// console.log(june)
+// console.log(amy.getFullName())
+// console.log(neo.getFullName())
